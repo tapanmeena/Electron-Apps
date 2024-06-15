8828e505f0fa47a39f2449ddc0e4533f5d488812
Tapan Meena <tapanmeena1998@gmail.com>
2024-06-16 01:00:01 +0530

    chore: Add Electron support for React app

---
+++ 2-Msal-Authentication/src/electron/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      contextIsolation: true,
+      enableRemoteModule: false,
+    },
+  });
+
+  mainWindow.loadURL("http://localhost:3000");
+//   mainWindow.loadURL(`file://${path.join(__dirname, "../../build/index.html")}`);
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  setupIpcHandlers();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
